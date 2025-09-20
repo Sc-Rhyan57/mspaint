b63cb1100210b34747653e58a668fcffc296c478
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-09-20 12:25:48 -0300

    NEW LOADSTRING

--- main.lua
+++ main.lua
@@ -1,4 +1,1 @@
---[[ CORRECTION (TY JACOB) ]]--
-game.ReplicatedStorage:WaitForChild("ModulesClient").Name = "ClientModules"
-loadstring(game:HttpGet("https://github.com/Sc-Rhyan57/mspaint/releases/download/3.8/Script.luau"))()
-
+loadstring(game:HttpGet("https://github.com/Sc-Rhyan57/mspaint/releases/download/3.8-FV/Script.luau"))()
