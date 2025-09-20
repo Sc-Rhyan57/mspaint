70319155be3bd0af157ce4e4733c83d6ca1bd09f
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-09-20 12:04:38 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,4 @@
+--[[ CORRECTION (TY JACOB) ]]--
+game.ReplicatedStorage:WaitForChild("ModulesClient").Name = "ClientModules"
 loadstring(game:HttpGet("https://github.com/Sc-Rhyan57/mspaint/releases/download/3.8/Script.luau"))()
 
